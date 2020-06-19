d0c69db29c8f38cc732ac36cf2ca290fa2e446c8
unknown <1012151538@qq.comgit config --global user.name >
2020-06-19 18:34:02 +0800

    should_return_2B_when_given_2356_answer_1234

--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -7,17 +7,7 @@ import org.mockito.stubbing.Answer;
 public class GuessNumberTest {
 
 
-    @Test
-    public void should_return_4A_when_given_1234_answer_1234()
-    {
-     //given
-       GamberAnswer answer=new GamberAnswer("1 2 3 4");
-       //when
-       String result =answer.check("1 2 3 4");
-       //then
-        Assert.assertEquals("4A",result);
 
-    }
 
     @Test
     public void should_return_4A0B_when_given_1234_answer_1234()
@@ -61,10 +51,15 @@ public class GuessNumberTest {
         //then
         Assert.assertEquals("1A1B",result);
     }
-   //数字全部对，位置全不对
     @Test
-    public void should_return_0A0B_when_given_not_position_not_number()
+    public void should_return_2B_when_given_2356_answer_1234()
     {
-
+        //given
+        GamberAnswer answer=new GamberAnswer("1 2 3 4");
+        //when
+        String result =answer.check("2 3 5 6");
+        //then
+        Assert.assertEquals("0A2B",result);
     }
+
 }
